507dc8b980689f1dca865c0d42d38c357db78351
sinapism <sheenaaoliveros@gmail.com>
2024-08-02 10:46:55 +0800

    delete

--- New folder/borrow/init-flatpickr.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Initialize Flatpickr for date range selection with manual input enabled
-    flatpickr("#dateRangeFilter", {
-        mode: "range",
-        dateFormat: "Y-m-d",
-        allowInput: true, // Enable manual input
-        onChange: filterAndRenderRecords
-    });
-});
