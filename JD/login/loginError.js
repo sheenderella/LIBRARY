db4e6c94a3ad916f3250ec904186aedfcd7bcbca
sinapism <sheenaaoliveros@gmail.com>
2024-12-13 10:12:19 +0800

    branching

--- JD/login/loginError.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const closeButton = document.querySelector('.close-button');
-
-    closeButton.addEventListener('click', () => {
-        window.close(); // Close the alert window
-    });
-});
--- JD/settings/support.js
+++
@@ -1,8 +0,0 @@
-// Sidebar toggle functionality
-document.getElementById('sidebarCollapse').addEventListener('click', function () {
-    const wrapper = document.getElementById('wrapper');
-    const sidebar = document.getElementById('sidebar-wrapper');
-    
-    wrapper.classList.toggle('collapsed');
-    sidebar.classList.toggle('collapsed');
-});
